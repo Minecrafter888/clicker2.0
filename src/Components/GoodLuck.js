ba24f6ff0f99ed218b0a184b687b24c7ba731a07
Minecrafter888 <118668882+Minecrafter888@users.noreply.github.com>
2023-12-05 13:44:48 +0100

    Delete src directory

--- src/Components/GoodLuck.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-function goodLuck(){
-    return(
-     <div style={{textAlign: "center"}}>
-        <p>Succes met werk thuis</p>
-     </div>
-    );
-}
-export default goodLuck
